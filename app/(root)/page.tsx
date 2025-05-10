ee0fe7127deee8d0e170ee63877f74ae7779f652
devsujalpatel <sujal7455@gmail.com>
2025-05-11 00:15:05 +0530

    videocard done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,25 @@
-import Header from '@/components/Header'
-import React from 'react'
+import Header from "@/components/Header";
+import VidoeCard from "@/components/VidoeCard";
+import React from "react";
 
 const Page = () => {
   return (
-    <main className='wrapper page'>
-      <Header title='All Videos' subHeader='Public Library' />
+    <main className="wrapper page">
+      <Header title="All Videos" subHeader="Public Library" />
       <h1>Hello, World!</h1>
+      <VidoeCard
+        id="1"
+        title="Snacast Message"
+        thumbnail="/assets/samples/thumbnail (1).png"
+        createdAt={new Date("2025-05-10")}
+        userImg="/assets/images/jason.png"
+        username="Sujal"
+        views={10}
+        visibility="public"
+        duration={156}
+      />
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
