3e0e17d4fdf658d97f337cd1b2562a1ca7725280
devsujalpatel <sujal7455@gmail.com>
2025-05-12 23:27:08 +0530

    video card created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,15 @@
 import Header from "@/components/Header";
-import VidoeCard from "@/components/VidoeCard";
+import VideoCard from "@/components/VideoCard";
 import React from "react";
 
 const Page = () => {
   return (
     <main className="wrapper page">
       <Header title="All Videos" subHeader="Public Library" />
-      <h1>Hello, World!</h1>
-      <VidoeCard
+      <VideoCard
         id="1"
         title="Snacast Message"
-        thumbnail="/assets/samples/thumbnail (1).png"
+        thumbnail="/assets/samples/thumbnail2.png"
         createdAt={new Date("2025-05-10")}
         userImg="/assets/images/jason.png"
         username="Sujal"
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 
-const VidoeCard = ({
+const VideoCard = ({
   id,
   title,
   thumbnail,
@@ -18,7 +18,7 @@ const VidoeCard = ({
       <Image
         src={thumbnail}
         alt="thumbnail"
-        width={290}
+        width={260}
         height={160}
         className="thumbnail"
       />
@@ -39,20 +39,31 @@ const VidoeCard = ({
             </figcaption>
           </figure>
           <aside>
-            <Image src="/assets/icons/eye.svg" alt="views" width={16} height={16} />
+            <Image
+              src="/assets/icons/eye.svg"
+              alt="views"
+              width={16}
+              height={16}
+            />
             <span>{views}</span>
           </aside>
         </div>
-        <h2>{title} - {" "} {createdAt.toLocaleDateString('en-IN', {
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric'
-        })}</h2>
+        <h2>
+          {title} -{" "}
+          {createdAt.toLocaleDateString("en-IN", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          })}
+        </h2>
       </article>
       <button onClick={() => {}} className="copy-btn">
         <Image src="/assets/icons/link.svg" alt="copy" width={18} height={18} />
       </button>
+      {duration && (
+        <div className="duration">{Math.ceil(duration / 60)}min</div>
+      )}
     </Link>
   );
 };
-export default VidoeCard;
+export default VideoCard;
